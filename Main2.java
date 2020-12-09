20ccfbbf489aaaf48e067187f13c55ce72dbcb60
Valeria19k <71520455+Valeria19k@users.noreply.github.com>
2020-12-09 15:19:52 +0300

    Create Main2.java

---
+++ Main2.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double elem;
+        System.out.println("The first 10 numbers of harmonic series:");
+       for(int i=1;i<11;i++){
+           elem=1.0/i;
+           System.out.println(elem);
+       }
+        // write your code here
+    }
+}
